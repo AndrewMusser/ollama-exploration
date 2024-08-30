237305a2ab7857eae1a69a25a5ced1450de83c36
Andrew Musser <andrew@musserautomation.com>
2024-08-30 12:56:21 -0400

    Add a simple LangChain agent

--- src/main.py
+++ src/main.py
@@ -1,49 +1,71 @@
-import ollama
-import streamlit as st
-# import torch
+from langchain_community.tools.tavily_search import TavilySearchResults
+search = TavilySearchResults()
+#result = search.invoke("what is the weather in SF")
 
-st.title("Ollama Python Chatbot")
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
+docs = loader.load()
+documents = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200
+).split_documents(docs)
+vector = FAISS.from_documents(documents, OpenAIEmbeddings())
+retriever = vector.as_retriever()
 
-# initialize history
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+from langchain.tools.retriever import create_retriever_tool
+retriever_tool = create_retriever_tool(
+    retriever,
+    "langsmith_search",
+    "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
+)
 
-# init models
-if "model" not in st.session_state:
-    st.session_state["model"] = ""
+# Import things that are needed generically
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain.tools import BaseTool, StructuredTool, tool
+@tool
+def time_tool() -> str:
+    """Use this tool to look up the current time on this workstation"""
+    import datetime
+    now = datetime.datetime.now()
+    return str(now)
 
-models = [model["name"] for model in ollama.list()["models"]]
-st.session_state["model"] = st.selectbox("Choose your model", models)
+tools = [search, retriever_tool, time_tool]
 
-def model_res_generator():
-    # if torch.cuda.is_available():
-    #     # Set the global PyTorch device to GPU
-    #     device = torch.device("cuda")
-    #     #torch.set_default_tensor_type("torch.cuda.FloatTensor")
-    # else:
-    #     # Use CPU if no GPU available
-    #     device = torch.device("cpu")
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
-    stream = ollama.chat(
-        model=st.session_state["model"],
-        messages=st.session_state["messages"],
-        stream=True,
+from langchain import hub
+# Get the prompt to use - you can modify this!
+prompt = hub.pull("hwchase17/openai-functions-agent")
+
+from langchain.agents import create_tool_calling_agent
+agent = create_tool_calling_agent(llm, tools, prompt)
+
+from langchain.agents import AgentExecutor
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+# response = agent_executor.invoke({"input": "what's the weather in SF?", "chat_history": []})
+# print(response)
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+message_history = ChatMessageHistory()
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    # This is needed because in most real world scenarios, a session id is needed
+    # It isn't really used here because we are using a simple in memory ChatMessageHistory
+    lambda session_id: message_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+while True:
+    request = input()
+    agent_with_chat_history.invoke(
+        {"input": request},
+        # This is needed because in most real world scenarios, a session id is needed
+        # It isn't really used here because we are using a simple in memory ChatMessageHistory
+        config={"configurable": {"session_id": "<foo>"}},
     )
-    for chunk in stream:
-        yield chunk["message"]["content"]
-
-# Display chat messages from history on app rerun
-for message in st.session_state["messages"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("Enter prompt here.."):
-    # add latest message to history in format {role, content}
-    st.session_state["messages"].append({"role": "user", "content": prompt})
-
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        message = st.write_stream(model_res_generator())
-        st.session_state["messages"].append({"role": "assistant", "content": message})
